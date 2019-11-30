414d885c087333101241f0f0944b49990ef1c054
紫幻 <31381762+zihuan1@users.noreply.github.com>
2019-11-30 15:46:36 +0800

    0.1-bate

--- vhlibrary/src/main/java/com/zihuan/utils/vhlibrary/ViewHelper.kt
+++ vhlibrary/src/main/java/com/zihuan/utils/vhlibrary/ViewHelper.kt
@@ -1,18 +1,25 @@
 package com.zihuan.utils.vhlibrary
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.view.View
 import android.widget.TextView
 
 /**
  *
- * @Description 简化了view一些操作
+ * @Description
  * @author zihuan
  * @date 2019/10/23 16:59
  */
 
+/**
+ * 将图片转换成指定颜色
+ */
+fun TransformImageColor(color: Int): PorterDuffColorFilter {
+    return PorterDuffColorFilter(color, PorterDuff.Mode.SRC_ATOP)
+}
 
 /***
- * 滑动改变色值
  *  scrollY 滑动的距离
  *  height 标题栏的高度
  *  return 透明度
@@ -22,29 +29,24 @@ fun viewAlphaChange(scrollY: Int, height: Int): Int {
         in 0..height -> { //滑动过程中 并且在height之内
             val scale = scrollY.toFloat() / height
             val alpha = 255 * scale
+//                Logger.tag("滑动中 scale $scale alpha $alpha")
             alpha.toInt()
         }
         else -> 255
     }
 }
 
-/**
- * 给TextView设置图片
- */
 fun TextView.setDrawablesBounds(start: Int = 0, tip: Int = 0, end: Int = 0, bottom: Int = 0) {
     setCompoundDrawablesRelativeWithIntrinsicBounds(start, tip, end, bottom)
 }
 
-/**
- * 隐藏view
- */
-fun View.VDismiss() {
+
+//隐藏view
+fun <T : View> T.VDismiss(): T {
     visibility = View.GONE
+    return this
 }
 
-/**
- * 隐藏多个view
- */
 fun VDismiss(vararg views: View): Array<View> {
     views.forEach {
         it.visibility = View.GONE
@@ -52,20 +54,16 @@ fun VDismiss(vararg views: View): Array<View> {
     return views as Array<View>
 }
 
-fun View.VDismiss(term: () -> Boolean) {
-    if (term()) VDismiss() else VShow()
-}
+fun <T : View> T.VDismiss(term: () -> Boolean) = if (term()) VDismiss() else VShow()
 
 /**
  * 显示当前view
  */
-fun View.VShow() {
+fun <T : View> T.VShow(): T {
     visibility = View.VISIBLE
+    return this
 }
 
-/**
- * 显示多个view
- */
 fun VShow(vararg views: View): Array<View> {
     views.forEach {
         it.visibility = View.VISIBLE
@@ -76,10 +74,7 @@ fun VShow(vararg views: View): Array<View> {
 /***
  * 传入一段lambda表达式来判断是否显示当前view
  */
-fun View.VShow(term: () -> Boolean) {
-    if (term()) VShow() else VDismiss()
-}
-
+fun <T : View> T.VShow(term: T.() -> Boolean) = if (term()) VShow() else VDismiss()
 
 fun Array<View>.indexOfShow(index: Int) {
     get(index).VShow()
@@ -104,3 +99,9 @@ fun Array<View>.lastShow() {
 fun Array<View>.lastDismiss() {
     indexOfDismiss(lastIndex)
 }
+
+//当前状态是否是显示
+fun View.VIsShow() = (visibility == View.VISIBLE)
+
+//当前状态是否隐藏
+fun View.VIsDismiss() = (visibility == View.GONE)
