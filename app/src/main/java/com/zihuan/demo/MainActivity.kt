942a4f24d007dc7449940c9f9e3c00a92639a269
紫幻 <zihuancz@foxmail.com>
2019-12-02 17:53:34 +0800

    Update MainActivity.kt

--- app/src/main/java/com/zihuan/demo/MainActivity.kt
+++ app/src/main/java/com/zihuan/demo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.zihuan.demo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.zihuan.utils.vhlibrary.VColor
+import com.zihuan.utils.vhlibrary.VInvisible
 import com.zihuan.utils.vhlibrary.VShow
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         tv_1.VShow()
+        tv_1.VInvisible()
         tv_1.VColor(R.color.colorAccent, R.color.colorPrimary, true)
     }
 }
